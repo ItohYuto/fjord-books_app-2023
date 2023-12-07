68e0e5a52fec6c2eb8b4fc9e8731a72293bd9d8d
Yuto <agbt.itohyuto@gmail.com>
2023-12-07 17:14:54 +0900

    ユーザ登録認証機能追加 ユーザ一覧画面ユーザー詳細画面追加 パスワード再設定メールの機能追加 日本語化対応

---
+++ app/models/user.rb
@@ -0,0 +1,22 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :profile, length: { maximum: 200 }
+  validates :username, :postal_code, :address, presence: true, on: :update
+  validates :postal_code, format: { with: /\A\d{7}\z/ }, on: :update
+
+  def update_without_current_password(params, *options)
+    if params[:password].blank? && params[:password_confirmation].blank?
+      params.delete(:password)
+      params.delete(:password_confirmation)
+    end
+
+    result = update(params, *options)
+    clean_up_passwords
+    result
+  end
+
+end
