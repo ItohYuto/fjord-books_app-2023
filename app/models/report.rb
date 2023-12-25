6bbc397178061c5beda7bd1f031db7c76484db57
Yuto <agbt.itohyuto@gmail.com>
2023-12-25 09:13:23 +0900

    言及機能追加
    日報の作成、編集、削除時に言及について更新される。
    日報の保存処理及び言及の保存処理は同じトランザクション内で実行しどちらかで失敗した場合、処理をロールバックする。
    日報が自身を言及することはできない。
    言及されている日報詳細ページでは言及している日報のリンクの一覧が表示される。

--- app/models/report.rb
+++ app/models/report.rb
@@ -7,11 +7,27 @@ class Report < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
 
+  has_many :mention_relationships, dependent: :destroy
+  has_many :mentioning_reports, through: :mention_relationships, source: :mention
+  has_many :reverse_of_relationships, class_name: 'MentionRelationship', foreign_key: 'mention_id', inverse_of: 'mention', dependent: :destroy
+  has_many :mentioned_reports, through: :reverse_of_relationships, source: :report
+
   def editable?(target_user)
     user == target_user
   end
 
   def created_on
     created_at.to_date
+  end
+
+  def mentions(target_reports)
+    target_reports.map { |report| mention_relationships.find_or_create_by(mention_id: report.id) if self != report }.compact
+  end
+
+  def mention_cancels(target_reports)
+    target_reports.map do |report|
+      mention_relationship = mention_relationships.find_by(mention_id: report.id)
+      mention_relationship&.destroy
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_02_082433) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_18_065809) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -59,6 +59,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_02_082433) do
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
+  create_table "mention_relationships", force: :cascade do |t|
+    t.integer "report_id"
+    t.integer "mention_id"
+    t.index ["mention_id"], name: "index_mention_relationships_on_mention_id"
+    t.index ["report_id", "mention_id"], name: "index_mention_relationships_on_report_id_and_mention_id", unique: true
+    t.index ["report_id"], name: "index_mention_relationships_on_report_id"
+  end
+
   create_table "reports", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "title", null: false
@@ -87,5 +95,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_02_082433) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "comments", "users"
+  add_foreign_key "mention_relationships", "reports"
+  add_foreign_key "mention_relationships", "reports", column: "mention_id"
   add_foreign_key "reports", "users"
 end
